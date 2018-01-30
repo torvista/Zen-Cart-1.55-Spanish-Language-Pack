7c345cfdafedd8e2059e35a7d959e66295c66bdb
Steve <torvista@users.noreply.github.com>
2018-01-30 13:03:17 +0100

    email header image is not language-dependant

--- files_usted/ADMIN_NAME/includes/languages/spanish/email_extras.php
+++ files_usted/ADMIN_NAME/includes/languages/spanish/email_extras.php
@@ -7,10 +7,10 @@
  * @version $Id: Author: DrByte  Wed Oct 28 13:08:12 2015 -0400 Modified in v1.5.5 $
  */
 
-  define ('EMAIL_LOGO_FILENAME', 'header.jpg');  //-File is present in /email folder
+  define ('EMAIL_LOGO_FILENAME', 'header.jpg');  //-File is present in /email folder NOT /es/email!! function zen_build_html_email_from_template does not take language into account
   define ('EMAIL_LOGO_WIDTH', '550');
   define ('EMAIL_LOGO_HEIGHT', '110');
-  define ('EMAIL_LOGO_ALT_TITLE_TEXT', 'Zen Cart! The Art of E-commerce');
+  define ('EMAIL_LOGO_ALT_TITLE_TEXT', '¡Zen Cart! El Arte de E-commerce');
   
   // -----
   // If you want to include some extra information in each email's header information (like perhaps the store address and/or phone number),
